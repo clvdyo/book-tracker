9a6a7e05ffbd65527c1cd16f043a779548f10231
A. Nurcahaya Tampubolon <anurcahayatampubolon@gmail.com>
2024-02-13 15:31:04 +0700

    tutorial 2 selesai

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_book, show_xml, show_json, show_xml_by_book_id, show_json_by_book_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create_book', create_book, name='create_book'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:book_id>/', show_xml_by_book_id, name='show_xml_by_book_id'),
+    path('json/<int:book_id>/', show_json_by_book_id, name='show_json_by_book_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import BookForm
+from main.models import Book
+from django.http import HttpResponse
+from django.core import serializers
+
 
 # Create your views here.
 def show_main(request):
+    books = Book.objects.all()
+
     context = {
         'name': 'A. Nurcahaya Tampubolon',
-        'class': 'PBP Genap'
+        'class': 'PBP A Genap',
+        'books': books
     }
 
     return render(request, "main.html", context)
+
+def create_book(request):
+    form = BookForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_book.html", context)
+
+def show_xml(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_book_id(request, book_id):
+    data = Book.objects.filter(pk=book_id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_book_id(request, book_id):
+    data = Book.objects.filter(pk=book_id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
